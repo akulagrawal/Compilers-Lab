5b26f39432b541a3d353963fd46372106965e9a6
Nitesh Jindal <niteshjindalxb@gmail.com>
2019-01-30 19:29:33 +0530

    Generating 3 address code for Arithmetic operations

--- Assignment_1/name.c
+++ Assignment_1/name.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include "lex.h"
+
 char  *Names[] = { "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7" };   
 char  **Namep  = Names;   
    
@@ -16,8 +19,8 @@ freename(s)
 char    *s;   
 {   
     if( Namep > Names )   
-    *--Namep = s;   
+       *--Namep = s;   
     else   
-    fprintf(stderr, "%d: (Internal error) Name stack underflow\n",   
+        fprintf(stderr, "%d: (Internal error) Name stack underflow\n",   
                                 yylineno );   
 }   
