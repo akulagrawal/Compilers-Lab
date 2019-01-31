a27f6a7a05834827c5a0f76fb338edbe575dad31
Ameya Daigavane <ameya.d.98@gmail.com>
2019-01-31 19:50:15 +0530

    Changed registers to memory locations - now, support for many more variables. Multiplication also added.

--- Assignment_1/assembly.py
+++ Assignment_1/assembly.py
@@ -3,13 +3,19 @@ Assembly Code Generation
 30th January, 2019
 '''
 
+# The maximum number of 8-bit variables (ie, memory locations) we will use.
+max_variables = 100
+
+# Memory start location
+memory_start = 8000
+
 # Path for the IR code text file that has to be translated.
 IR_file_path = "IR.txt"
 
-# Define virtual registers, real registers and the mapping between them.
-virtual_registers = ["_t" + chr(num + ord('0')) for num in range(7)]
-registers = [chr(ord('A') + 1 + num) for num in range(7)]
-register_map = dict(zip(virtual_registers, registers))
+# Define virtual registers, real memory locations and the mapping between them.
+virtual_registers = ["_t" + chr(num + ord('0')) for num in range(max_variables)]
+memory_locations = [str(8000 + num) + 'H' for num in range(max_variables)]
+memory_map = dict(zip(virtual_registers, memory_locations))
 
 
 # Returns the hexadecimal equivalent of num (possibly, a string) in Assembly format.
@@ -17,54 +23,131 @@ def to_hex(num):
     return hex(int(num))[2:].upper() + 'H'
 
 
-# Instructions Class.
-class Instruction:
+# Processor Class.
+class Processor:
+    def __init__(self):
+        self.num_registers = 6
+        self.registers = ['B', 'C', 'D', 'E', 'H', 'L']
+        self.free_registers = self.registers.copy()
+        self.busy_registers = []
+        self.curr_loop = 0
+
+    # Assign a free register.
+    def get_free_register(self):
+        # No free registers available.
+        if len(self.free_registers) == 0:
+            raise IndexError('No free registers left.')
+
+        free_register = self.free_registers.pop(0)
+        self.busy_registers.append(free_register)
+        return free_register
+
+    # Deallocate a previously busy register.
+    def return_register(self, register_name):
+        if register_name not in self.busy_registers:
+            raise NameError('Incorrect register name.')
+
+        self.busy_registers.remove(register_name)
+        self.free_registers.append(register_name)
+
     @staticmethod
     def assign(operand1, operand2):
+        # Load into accumulator.
         if operand2 in virtual_registers:
-            print("MOV " + register_map[operand1] + ", " + register_map[operand2])
+            print("LDA " + memory_map[operand2])
         else:
-            print("MVI " + register_map[operand1] + ", " + to_hex(operand2))
+            print("MVI " + "A" + ", " + to_hex(operand2))
+
+        # Store into memory location,
+        print("STA " + memory_map[operand1])
 
     @staticmethod
     def is_assignment(tokenlist):
         return tokenlist[1] == "="
 
-    @staticmethod
-    def add(operand1, operand2):
-        # Move to accumulator
-        print("MOV " + "A" + ", " + register_map[operand1])
+    # Add 8-bit operands.
+    def add(self, operand1, operand2):
+        # Load into accumulator.
+        print("LDA " + memory_map[operand1])
 
-        # Add to accumulator
+        # Add to accumulator, using a free register if not immediate data.
         if operand2 in virtual_registers:
-            print("ADD " + register_map[operand2])
+            free_register = self.get_free_register()
+            print("MOV " + free_register + ", " + "A")
+            print("LDA " + memory_map[operand2])
+            print("ADD " + free_register)
+            self.return_register(free_register)
         else:
             print("ADI " + to_hex(operand2))
 
-        # Shift back to register
-        print("MOV " + register_map[operand1] + ", " + "A")
+        # Store into memory location.
+        print("STA " + memory_map[operand1])
 
     @staticmethod
     def is_addition(tokenlist):
         return tokenlist[1] == "+="
 
-    @staticmethod
-    def sub(operand1, operand2):
-        # Move to accumulator
-        print("MOV " + "A" + ", " + register_map[operand1])
-
-        # Add to accumulator
+    # Subtract with 8-bit operands.
+    def sub(self, operand1, operand2):
+        # Load into accumulator.
         if operand2 in virtual_registers:
-            print("SUB " + register_map[operand2])
+            print("LDA " + memory_map[operand2])
         else:
-            print("SUI " + to_hex(operand2))
+            print("MVI " + to_hex(operand2))
 
-        # Shift back to register.
-        print("MOV " + register_map[operand1] + ", " + "A")
+        # Subtract from accumulator, using a free register.
+        free_register = self.get_free_register()
+        print("MOV " + free_register + ", " + "A")
+        print("LDA " + memory_map[operand1])
+        print("SUB " + free_register)
+        self.return_register(free_register)
+
+        # Store into memory location.
+        print("STA " + memory_map[operand1])
 
     @staticmethod
     def is_subtraction(tokenlist):
         return tokenlist[1] == "-="
+
+    # Multiply 4-bit operands.
+    def mul(self, operand1, operand2):
+        # Load into accumulator.
+        if operand2 in virtual_registers:
+            print("LDA " + memory_map[operand2])
+        else:
+            print("MVI " + "A" + ", " + to_hex(operand2))
+
+        # Check if second operand is 0.
+        self.curr_loop += 1
+        print("CPI 0H")
+        print("JNZ " + "NZINP" + str(self.curr_loop))
+        print("STA " + memory_map[operand1])
+
+        # If the second operand isn't zero, continue.
+        print("NZINP" + str(self.curr_loop) + ": NOP")
+
+        # Move the second operand to a free register, after decrementing.
+        print("DCR A")
+        operand2_register = self.get_free_register()
+        print("MOV " + operand2_register + ", " + "A")
+
+        # Load first operand and move to a free register.
+        print("LDA " + memory_map[operand1])
+        operand1_register = self.get_free_register()
+        print("MOV " + operand1_register + ", " + "A")
+
+        # Addition loop - multiplication by repeated addition.
+        print("LOOP" + str(self.curr_loop) + ": NOP")
+        print("LDA " + memory_map[operand1])
+        print("ADD " + operand1_register)
+        print("STA " + memory_map[operand1])
+
+        # Check the value of the second operand, after decrementing.
+        print("DCR " + operand2_register)
+        print("MOV " + "A" + ", " + operand2_register)
+
+        # Loop while the second operand is still non-zero.
+        print("JNZ " + "LOOP" + str(self.curr_loop))
 
     @staticmethod
     def is_multiplication(tokenlist):
@@ -76,6 +159,10 @@ class Instruction:
 
 
 if __name__ == "__main__":
+
+    # Create processor object.
+    processor = Processor()
+
     with open(IR_file_path, "r") as f:
         # Read all lines from the file.
         lines = f.readlines()
@@ -85,17 +172,17 @@ if __name__ == "__main__":
             tokens = line.split()
 
             # Check operation.
-            if Instruction.is_assignment(tokens):
-                Instruction.assign(tokens[0], tokens[2])
+            if processor.is_assignment(tokens):
+                processor.assign(tokens[0], tokens[2])
 
-            elif Instruction.is_addition(tokens):
-                Instruction.add(tokens[0], tokens[2])
+            elif processor.is_addition(tokens):
+                processor.add(tokens[0], tokens[2])
 
-            elif Instruction.is_subtraction(tokens):
-                Instruction.sub(tokens[0], tokens[2])
+            elif processor.is_subtraction(tokens):
+                processor.sub(tokens[0], tokens[2])
 
-            elif Instruction.is_multiplication(tokens):
-                Instruction.mul(tokens[0], tokens[1])
+            elif processor.is_multiplication(tokens):
+                processor.mul(tokens[0], tokens[2])
 
-            elif Instruction.is_division(tokens):
-                Instruction.div(tokens[0], tokens[1])
+            elif processor.is_division(tokens):
+                processor.div(tokens[0], tokens[2])
