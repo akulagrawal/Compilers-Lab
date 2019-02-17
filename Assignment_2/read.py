023993324d17b3dc16e36d51716d9384b0f69692
Akul Agrawal <31360523+akulagrawal@users.noreply.github.com>
2019-02-17 22:39:44 +0530

    Object declaration working

--- Assignment_2/read.py
+++ Assignment_2/read.py
@@ -4,11 +4,19 @@ f = open(sys.argv[1]).read()
 lines = f.splitlines()
 
 #number of class declarations
-decclass=0
+decobj=0
 
 for x in lines:
-	if(re.match(r"[ \t]*(public[ \t]+|private[ \t]+)?class[ \t]+((extends)[ \t]+([a-zA-Z][a-zA-Z0-9_]*)[ \t]+(implements)[ \t]+([a-zA-Z][a-zA-Z0-9_]*)[ \t]*|(implements)[ \t]+([a-zA-Z][a-zA-Z0-9_]*)[ \t]*|(extends)[ \t]+([a-zA-Z][a-zA-Z0-9_]*)[ \t]*|([a-zA-Z][a-zA-Z0-9_]*)[ \t]*)",x)):
-		decclass += 1
+	line = []
+	flag = 0
+	for word in x.split():
+		if re.search(word + r"[\s]*([a-zA-Z][a-zA-Z0-9_]*)[\s]*(\s=\s)[\s]*(new)[\s]*" + word + r"(\()",x):
+			flag = 1
+			print(x)
+			break
+	if re.search(r"(.newInstance\()",x) or re.search(r"(.readObject\()",x):
 		print(x)
+		flag = 1
+	decobj += flag
 
-print(decclass)
+print(decobj)
