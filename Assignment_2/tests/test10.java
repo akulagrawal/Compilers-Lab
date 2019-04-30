92fe25f7b76cf69813eebe686b37f868222b7d85
Akul Agrawal <akulagrawal@rediffmail.com>
2019-05-01 00:41:10 +0530

    Restore Assignment 2

---
+++ Assignment_2/tests/test10.java
@@ -0,0 +1,10 @@
+/*
+Lines with...
+Objects: 0
+Classes: 1
+Constructors: 1
+Inherited Classes: 0
+*/
+
+
+public class HelloWorld{ HelloWorld() { System.out.println("Hello World"); } HelloWorld(int a) { System.out.println("Hello World"); }public static void main(String []args){ System.out.println("Hello World"); }} public class HelloWorld1{ HelloWorld1() { System.out.println("Hello World"); } HelloWorld1(int a) { System.out.println("Hello World"); }public static void main(String []args){ System.out.println("Hello World"); }} public class HelloWorld3{ HelloWorld3() { System.out.println("Hello World"); } HelloWorld3(int a) { System.out.println("Hello World"); }public static void main(String []args){ System.out.println("Hello World"); }}
---
+++ Assignment_2/tests/test5.java
@@ -0,0 +1,32 @@
+/*
+Lines with...
+Objects: 3
+Classes: 1
+Constructors: 2
+Inherited Classes: 0
+*/
+
+class Company {
+
+    String domainName;
+
+    public Company(){
+        this.domainName = "default";
+    }
+
+    public Company(String domainName){
+        this.domainName = domainName;
+    }
+
+    public void getName(){
+        System.out.println(this.domainName);
+    }
+
+    public static void main(String[] args) {
+        Company defaultObj = new Company();
+        Company programizObj = new Company("programiz.com");
+
+        defaultObj.getName();
+        programizObj.getName();
+    }
+}
